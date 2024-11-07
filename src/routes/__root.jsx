376f6aa8ee1fe171c9402db16f27faa22262482e
Quls <Yudriqul@0803@gmail.com>
2024-11-07 12:10:18 +0700

    featur search cars

--- src/routes/__root.jsx
+++ src/routes/__root.jsx
@@ -1,8 +1,6 @@
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
-import { Container } from "react-bootstrap";
-import NavigationBar from "./components/Navbar";
-// import NavigationBar from "../components/Navbar";
+import NavigationBar from "../components/Navbar";
 
 export const Route = createRootRoute({
   component: () => (
