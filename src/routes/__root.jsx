6532f9ce0e111c3ad6616fb27aea4bfefa8410a3
pentahead <dhiyaafaruq@gmail.com>
2024-11-09 02:30:24 +0700

    update: card appereance

--- src/routes/__root.jsx
+++ src/routes/__root.jsx
@@ -2,6 +2,9 @@ import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { ToastContainer } from "react-bootstrap";
+import "react-toastify/dist/ReactToastify.css";
+import { confirmAlert } from "react-confirm-alert"; // Import
+import "react-confirm-alert/src/react-confirm-alert.css";
 export const Route = createRootRoute({
   component: () => (
     <>
